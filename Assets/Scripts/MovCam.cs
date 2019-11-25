50d061bbf8516b100543bd044cf9c330d59ba5ba
Taras <tariksyurts@gmail.com>
2019-11-25 14:41:38 +0200

    Initial

---
+++ Assets/Scripts/MovCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 5);
+            print("kek");
+        }
+        
+    }
+}
