183b46c0ff65ae7f10413d316d673fa25a62fa0e
Myh45 <54940009+Myh45@users.noreply.github.com>
2019-11-27 20:57:28 +0200

    Setup camera to follow player character

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform playerObject;
+
+    public float distanceFromObject = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookOnObject = playerObject.position - transform.position;
+
+        lookOnObject = playerObject.position - transform.position;
+
+        transform.forward = lookOnObject.normalized;
+
+
+        Vector3 playerLastPosition;
+        playerLastPosition = playerObject.position - lookOnObject.normalized * distanceFromObject;
+
+        playerLastPosition.y = playerObject.position.y + distanceFromObject / 2;
+
+        transform.position = playerLastPosition;
+
+
+    }
+}
